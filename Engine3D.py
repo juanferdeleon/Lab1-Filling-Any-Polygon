8f6a2267153b8eaa4d5a572260569c07ad81a039
Juan De Leon <dele17822@uvg.edu.gt>
2020-07-26 14:42:41 -0600

    Initial commit

---
+++ Engine3D.py
@@ -0,0 +1,84 @@
+'''
+        SR1 Points
+
+Creado por:
+
+    Juan Fernando De Leon Quezada   Carne 17822
+
+Engine 3D
+
+'''
+
+from gl import Bitmap
+
+bmp = Bitmap(600, 600)
+
+def glInit():
+    return bmp
+
+
+if __name__ == '__main__':
+    '''Main Program'''
+
+    #Initialize bmp Object
+    bmp = glInit()
+
+    #Input bmp width and height
+    while True:
+        bmp_width = input("\nIngrese ancho de la imagen: ")
+        bmp_height = input("\nIngrese alto de la imagen: ")
+        try:
+            bmp_width = int(bmp_width)
+            bmp_height = int(bmp_height)
+            break
+        except ValueError:
+            print("\n\nERROR: Por favor ingrese un numero entero.\n")
+
+    #Set bmp width and height
+    bmp.glCreateWindow(bmp_height, bmp_width)
+
+    #Input bmp Viewport width and height
+    while True:
+        bmp_viewport_x = input("\nIngrese coordenada X para el Viewport: ")
+        bmp_viewport_y = input("\nIngrese coordenada Y para el Viewport: ")
+        bmp_viewport_width = input("\nIngrese ancho del ViewPort: ")
+        bmp_viewport_height = input("\nIngrese alto del ViewPort: ")
+        try:
+            bmp_viewport_x = int(bmp_viewport_x)
+            bmp_viewport_y = int(bmp_viewport_y)
+            bmp_viewport_width = int(bmp_viewport_width)
+            bmp_viewport_height = int(bmp_viewport_height)
+            break
+        except ValueError:
+            print("\n\nERROR: Por favor ingrese un numero entero.\n")
+
+    #Set bmp Viewport width and height
+    bmp.glViewPort(bmp_viewport_x, bmp_viewport_y, bmp_viewport_height, bmp_viewport_width)
+
+    #Set all pixels to same color
+    bmp.glClear()
+
+    #Input glVertex Colors
+    while True:
+        bmp_r = input("\nHex color (R): ")
+        bmp_g = input("\nHex color (G): ")
+        bmp_b = input("\nHex color (B): ")
+        try:
+            bmp_r = float(bmp_r)
+            bmp_g = float(bmp_g)
+            bmp_b = float(bmp_b)
+            if 0 <= bmp_g <= 1 and 0 <= bmp_g <= 1 and 0 <= bmp_b <= 1:
+                break
+        except ValueError:
+            print("\n\nERROR: Por favor ingrese un numero entre 0 y 1.\n")
+
+    #Input glVertex Colors
+    bmp.glColor(bmp_r, bmp_g, bmp_b)
+
+    bmp.glLine(-1, -1, 1, 1)
+    bmp.glLine(-1, 1, 1, -1)
+    bmp.glLine(0, -1, 0, 1)
+    bmp.glLine(-1, 0, 1, 0)
+
+    #Output BMP
+    bmp.glWrite("test.bmp")
---
+++ README.md
@@ -0,0 +1,15 @@
+# SR2: Lines
+
+SR2 es un algoritmo que permite dibujar una línea en un archivo bmp, similar a su implementación de glPoint() en [SR1: Points](https://github.com/juanferdeleon/SR1-Points).
+
+## Como ejecutar el codigo
+
+Descargue el repositorio [SR2: Lines](https://github.com/juanferdeleon/SR2-Lines)
+
+Luego ejecute el archivo [Engine3D.py](/Engine3D.py) dentro de cualquier IDE.
+
+Si lo ejecuta desde la linea de comandos:
+
+```bash
+python Engine3D.py
+```
